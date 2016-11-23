11896bd500bdc27c0788db0b22a6ddcfa6fe807f
John Yom <johnyom87@gmail.com>
2016-11-23 12:44:24 -0500

    added restaurant and activity

--- public/js/main.js
+++ public/js/main.js
@@ -23,7 +23,7 @@ function addHotel() {
         if (!listItem || listItem.length === 0) {
 
             // following lines are sending POST request to the server.
-            // which means you can recieve this POST request from express router. 
+            // which means you can recieve this POST request from express router.
             // kind of similar to Sequalize querying right?
             // window.location.href is used to get current url from broweser
 
@@ -32,7 +32,7 @@ function addHotel() {
                 day: day
             }, function(hotel) {
                 // result of Async POST call is now a Sequelize instance pass by express response.json();
-                console.log(hotel);    
+                console.log(hotel);
 
                 let $template = $(''+
                   '<div class="list-item row">' +
@@ -54,13 +54,59 @@ function addHotel() {
 function addRestaurant() {
     console.log("Restaurant button is clicked");
 
-    var day, selectedVal = $('#hotel-list').val()
+    var day;
+    // $element = $('#restaurant-list');
+    //gave us the ID
+    var selectedVal = $('#restaurant-list').val();
+    //this gives us the name of the restaurant
+    //label option tag text
+  var label = $('#restaurant-list').find('option:selected').text()
 
+  $.when(
+      // here we are running through each DOM node with class '.day-tab'. Look at line 76 on /views/trip.html
+      $('.tab-link').each(function() {
+          if ($(this).hasClass('active')) {
+              day = $(this).data('value');
+          }
+      })
 
+  ).done(function() {
+      let $template = $(''+
+        '<div class="list-item row">' +
+          '<div class="col s10">'+label+'</div>'+
+          '<div class="col s2">'+
+            '<a class="clear-restaurant" data-value="'+selectedVal+'">'+
+              '<i class="material-icons">clear</i>'+
+            '</a>'+
+          '</div>'+
+        '</div>');
+      $('#day'+day+'>#restaurant-plan>.plan-list').append($template);
+    })
 }
 
 function addActivity() {
-
+    console.log("Activity button is clicked");
+    var day;
+    var selectedVal = $('activity-list').val();
+    var label = $('#activity-list').find('option:selected').text();
+    $.when(
+          $('.tab-link').each(function(){
+            if($(this).hasClass('active')){
+                day = $(this).data('value');
+            }
+          })
+    ).done(function(){
+        let $template = $(''+
+          '<div class="list-item row">' +
+            '<div class="col s10">'+label+'</div>'+
+            '<div class="col s2">'+
+              '<a class="clear-activity" data-value="'+selectedVal+'">'+
+                '<i class="material-icons">clear</i>'+
+              '</a>'+
+            '</div>'+
+          '</div>');
+        $('#day'+ day+'>#activities-plan >.plan-list').append($template);
+    })
 }
 
 $(document).ready(function() {
